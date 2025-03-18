3b2dc8ce359b437247768e8f8b62623fbcbe2ecb
MOHAMMEDHASEEB1 <mohammedhaseeb112006@gmail.com>
2025-03-18 21:17:24 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+echo "Enter Principal Amount:"  
+read principal  
+echo "Enter Rate of Interest:"  
+read rate  
+echo "Enter Time (in years):"  
+read time  
+interest=$(( (principal * rate * time) / 100 ))  
+echo "Simple Interest: $interest"  
